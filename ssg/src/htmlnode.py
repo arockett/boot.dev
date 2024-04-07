263dbcdc098ff639029e76baae9b882d204f4cd7
Aaron Beckett <rockettman.labs@gmail.com>
2024-04-07 01:27:44 -0600

    ssg: add LeafNode class

--- ssg/src/htmlnode.py
+++ ssg/src/htmlnode.py
@@ -25,3 +25,15 @@ class HTMLNode:
         ]
         return ", ".join(parts)
 
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("All leaf nodes require a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
--- ssg/src/test_htmlnode.py
+++ ssg/src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -14,6 +14,34 @@ class TestHTMLNode(unittest.TestCase):
     def test_empty_props(self):
         node = HTMLNode()
         self.assertEqual(node.props_to_html(), "")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_html(self):
+        node = LeafNode("p", "This is a paragraph")
+        self.assertEqual(
+            node.to_html(),
+            "<p>This is a paragraph</p>"
+        )
+
+    def test_html_with_props(self):
+        node = LeafNode("a", "Link", {"href": "http://example.com"})
+        self.assertEqual(
+            node.to_html(),
+            "<a href=\"http://example.com\">Link</a>"
+        )
+
+    def test_no_tag(self):
+        node = LeafNode(None, value="Just some text")
+        self.assertEqual(
+            node.to_html(),
+            "Just some text"
+        )
+
+    def test_no_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("p", None)
+            node.to_html()
 
 
 if __name__ == "__main__":
