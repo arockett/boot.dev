dea99835693ab03e1529e8eecf7b2c715225aef6
Aaron Beckett <rockettman.labs@gmail.com>
2024-04-12 22:36:10 -0600

    ssg: add parsing for text to inline markdown

--- ssg/src/test_inline_markdown.py
+++ ssg/src/test_inline_markdown.py
@@ -15,6 +15,7 @@ from inline_markdown import (
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 
@@ -201,6 +202,25 @@ class TestInlineMarkdown(unittest.TestCase):
     def test_split_no_link_node(self):
         node = TextNode("no links here", text_type_text)
         self.assertEqual(split_nodes_link([node]), [node])
+
+    def test_text_to_textnodes(self):
+        self.assertEqual(
+            text_to_textnodes(
+                "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
+            ),
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ]
+        )
 
 
 if __name__ == "__main__":
