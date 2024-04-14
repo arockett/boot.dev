cca52b0b13bf4bf972e315a3ac83e78a70b4a754
Aaron Beckett <rockettman.labs@gmail.com>
2024-04-13 23:07:45 -0600

    ssg: detect block types

--- ssg/src/markdown_blocks.py
+++ ssg/src/markdown_blocks.py
@@ -1,4 +1,44 @@
+import re
+
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ulist = "unordered_list"
+block_type_olist = "ordered_list"
+
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = [b.strip() for b in markdown.split("\n\n")]
     return [b for b in blocks if len(b) > 0]
+
+def block_to_block_type(block: str) -> str:
+    lines = block.split("\n")
+    if re.match(r"#{1,6} .+", block) is not None:
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
--- ssg/src/test_markdown_blocks.py
+++ ssg/src/test_markdown_blocks.py
@@ -1,9 +1,18 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_ulist,
+    block_type_olist,
+)
 
 
-class TestBlockMarkdown(unittest.TestCase):
+class TestMarkdownBlocks(unittest.TestCase):
     def test_block_splitting1(self):
         markdown = '''
 # This is a heading
@@ -39,6 +48,54 @@ This is the same paragraph on a new line
                 "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
                 "* This is a list\n* with items"
             ]
+        )
+
+    def test_detect_paragraph_block(self):
+        self.assertEqual(
+            block_to_block_type("Simple paragraph"),
+            block_type_paragraph
+        )
+        self.assertEqual(
+            block_to_block_type("A paragraph\nwith multiple\nlines"),
+            block_type_paragraph
+        )
+
+    def test_detect_heading_block(self):
+        self.assertEqual(
+            block_to_block_type("# Header"),
+            block_type_heading
+        )
+        self.assertEqual(
+            block_to_block_type("## Header"),
+            block_type_heading
+        )
+
+    def test_detect_code_block(self):
+        self.assertEqual(
+            block_to_block_type("```\nThis is some code\n```"),
+            block_type_code
+        )
+
+    def test_detect_quote_block(self):
+        self.assertEqual(
+            block_to_block_type(">This is a\n>block quote"),
+            block_type_quote
+        )
+
+    def test_detect_list_block(self):
+        self.assertEqual(
+            block_to_block_type("* item one\n* item two\n* item three"),
+            block_type_ulist
+        )
+        self.assertEqual(
+            block_to_block_type("- item one\n- item two\n- item three"),
+            block_type_ulist
+        )
+
+    def test_detect_ordered_list_block(self):
+        self.assertEqual(
+            block_to_block_type("1. item one\n2. item two\n3. item three"),
+            block_type_olist
         )
 
 
