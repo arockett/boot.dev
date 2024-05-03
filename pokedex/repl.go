a7a397a3a1aa15292cbf9919ce0c4b454adb4fab
Aaron Beckett <rockettman.labs@gmail.com>
2024-05-03 09:27:14 -0600

    pokedex: add pokedex command

--- pokedex/repl.go
+++ pokedex/repl.go
@@ -93,5 +93,10 @@ func getCommands() map[string]cliCommand {
 			description: "Display information about a Pokemon",
 			callback:    commandInspect,
 		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "List all pokemon you've caught",
+			callback:    commandPokedex,
+		},
 	}
 }
